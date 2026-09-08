aa5b37f48ad115c4ce5ec0151e4eef931a863386
Devs2-Stores <individual.devs2@gmail.com>
2026-09-08 16:25:28 +0700

    Revert "fix(site-clone): exempt standard hyperlink navigation hrefs in HTML from lingering sub-resource audit"
    This reverts commit 3ff554adadf9cabe634f43604c08bbb10e77726f.

--- packages/site-clone/src/models/asset-localizer.ts
+++ packages/site-clone/src/models/asset-localizer.ts
@@ -1267,7 +1267,6 @@ export class AssetLocalizer {
     const seenFileUrlPairs = new Set<string>();
     const resourceUrlPattern = /(?:https?:)?\/\/[^\s"'`<>{}|\\^]+/gi;
     const namespaceAttrPattern = /\b(?:xmlns(?::\w+)?|itemtype|vocab)\s*=\s*["'][^"']*$/i;
-    const anchorHrefPattern = /\b(?:href)\s*=\s*["'][^"']*$/i;
 
     for (const file of options.rewrittenFiles) {
       let rMatch: RegExpExecArray | null;
@@ -1276,8 +1275,6 @@ export class AssetLocalizer {
         const prefix = file.rewrittenContent.slice(Math.max(0, rMatch.index - 50), rMatch.index);
         // Syntactic namespace exclusion: only exempt when immediately preceded by xmlns=, itemtype=, or vocab=
         if (namespaceAttrPattern.test(prefix)) continue;
-        // Hyperlink href exclusion: navigating URLs (<a href="...">) do not fetch remote network sub-resources on page load
-        if (file.path.endsWith('.html') && anchorHrefPattern.test(prefix) && !matchedUrl.includes('.css') && !matchedUrl.includes('.js')) continue;
 
         const dedupKey = `${file.path}::${matchedUrl}`;
         if (seenFileUrlPairs.has(dedupKey)) continue;
